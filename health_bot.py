3ecf5b5c31cac7599ad7bd17793b1b766344ba9c
Filinova Marina <filinugu@gmail.com>
2022-04-04 22:54:22 +0300

    test
    Signed-off-by: Filinova Marina <filinugu@gmail.com>

--- health_bot.py
+++ health_bot.py
@@ -303,7 +303,7 @@ def send_test(message):
     cur_thread = db_engine.get_cursor()
     cur_thread.execute(f'''SELECT * FROM activity WHERE user_id={message.from_user.id} 
                                                     AND chat_id={message.chat.id} 
-                                                    ORDER BY date DESC LIMIT 1''')
+                                                    ORDER BY date ASC LIMIT 1''')
 
     user_last_activity = cur_thread.fetchone()
     bot.reply_to(message, f'{user_last_activity}')
